b419a6f6fc826fa0939b9ae2e7017b0d28636112
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-03-15 09:41:12 +0800

    Update launch_benchmark.sh

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -30,6 +30,9 @@ function main {
     # generate benchmark
     for model_name in ${model_name_list[@]}
     do
+        if [ $batch_size -le 0 ];then
+            batch_size=64
+        fi
         # pre run
         python generate.py --outdir=tmp_output --trunc=0.7 --network=ffhq_256.pkl \
             --render-program=rotation_camera --seeds 1 \
