c8739003164bd90d31cdf210e3b18778da2d5dbd
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-03-18 12:14:31 +0800

    Update launch_benchmark.sh

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -31,7 +31,7 @@ function main {
     for model_name in ${model_name_list[@]}
     do
         if [ $batch_size -le 0 ];then
-            batch_size=64
+            batch_size=32
         fi
         # pre run
         python generate.py --outdir=tmp_output --trunc=0.7 --network=ffhq_256.pkl \
